defa34263623d1f665fe199ed736036939fea86e
Guilherme <guilherme.acra@usp.br>
2016-12-09 23:43:30 -0200

    Início da interface gráfica
    Tela inicial feita. Base das telas seguintes.

--- ProjetoBD/src/projetobd/ProjetoBD.java
+++ ProjetoBD/src/projetobd/ProjetoBD.java
@@ -1,12 +1,28 @@
 package projetobd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ProjetoBD{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        // TODO code application logic here
+        Connection connection;
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@grad.icmc.usp.br:15215:orcl",
+                    "8910542",
+                    "bd2016");
+            IntGraf intGraf = new IntGraf(connection);
+            intGraf.setVisible(true);
+        }catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return;
     }
     
 }
